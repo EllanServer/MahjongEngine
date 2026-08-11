c191da2da5e261a4c4f51781cbd0cb0f0708dd4a
Arbousier1 <elderli@foxmail.com>
2026-08-11 22:26:02 +0800

    fix: import trustee state assertion

--- modules/mahjong-application/src/test/java/top/ellan/mahjong/application/table/actor/TableActorTest.java
+++ modules/mahjong-application/src/test/java/top/ellan/mahjong/application/table/actor/TableActorTest.java
@@ -3,6 +3,7 @@ package top.ellan.mahjong.application.table.actor;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.nio.ByteBuffer;
 import java.time.Clock;
